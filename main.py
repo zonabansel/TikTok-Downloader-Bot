09f02f628496e34e74db62eb6a865613acf487c5
zonabansel <114482374+zonabansel@users.noreply.github.com>
2022-09-29 10:17:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ api = int(os.environ.get('API_KEY'))
 hash = os.environ.get('API_HASH')
 chnnl = os.environ.get('CHANNEL_URL')
 BOT_URL = os.environ.get('BOT_URL')
-app = Client("JayBee", bot_token=bot_token, api_id=api, api_hash=hash, workers=workers)
+app = Client("JayBee", bot_token=bot_token, api_id=api, api_hash=hash)
 
 
 
