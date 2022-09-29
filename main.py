827943c05954a7260ddc65ebd344e35dbfeab9c7
zonabansel <114482374+zonabansel@users.noreply.github.com>
2022-09-29 10:29:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -20,12 +20,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 bot_token = os.environ.get('BOT_TOKEN')
-workers = int(os.environ.get('4'))
+#workers = int(os.environ.get('4'))
 api = int(os.environ.get('API_KEY'))
 hash = os.environ.get('API_HASH')
 chnnl = os.environ.get('CHANNEL_URL')
 BOT_URL = os.environ.get('BOT_URL')
-app = Client("JayBee", bot_token=bot_token, api_id=api, api_hash=hash, workers=workers)
+app = Client("JayBee", bot_token=bot_token, api_id=api, api_hash=hash)
 
 
 
