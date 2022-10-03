0e88f30575dbae0ce361c163d63daa8fd9590aa8
zonabansel <114482374+zonabansel@users.noreply.github.com>
2022-10-03 13:02:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-import os
-import asyncio
-from pyrogram import Client
+# Copyright 2021 TerminalWarlord under the terms of the MIT
+# license found at https://github.com/TerminalWarlord/TikTok-Downloader-Bot/blob/master/LICENSE
+# Encoding = 'utf-8'
+# Fork and Deploy, do not modify this repo and claim it yours
+# For collaboration mail me at dev.jaybee@gmail.com
+from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, InlineQueryResultArticle, InputTextMessageContent
 import shutil
 import requests
 import json
+import os
 import re
 from bs4 import BeautifulSoup as bs
 import time
@@ -13,6 +17,7 @@ import math
 import base64
 from progress_bar import progress, TimeFormatter, humanbytes
 from dotenv import load_dotenv
+
 load_dotenv()
 bot_token = os.environ.get('BOT_TOKEN')
 workers = int(os.environ.get('WORKERS'))
@@ -70,7 +75,7 @@ def tiktok_dl(client, message):
     ### Get your Free TikTok API from https://rapidapi.com/TerminalWarlord/api/tiktok-info/
     #Using the default one can stop working any moment 
     
-    api = "https://tiktok-info.p.rapidapi.com/dl/"
+    api = f"https://tiktok-info.p.rapidapi.com/dl/"
     r = requests.get(api, params=params, headers=headers).json()['videoLinks']['download']
     directory = str(round(time.time()))
     filename = str(int(time.time()))+'.mp4'
@@ -82,7 +87,7 @@ def tiktok_dl(client, message):
         pass
     with requests.get(r, timeout=(50, 10000), stream=True) as r:
         r.raise_for_status()
-        with open('./{directory}/{filename}', 'wb') as f:
+        with open(f'./{directory}/{filename}', 'wb') as f:
             chunk_size = 1048576
             dl = 0
             show = 1
@@ -94,24 +99,24 @@ def tiktok_dl(client, message):
                     percent = 100
                 if show == 1:
                     try:
-                        a.edit('__**URL :**__ __{message.text}__\n'
-                               '__**Total Size :**__ __{total_size} MB__\n'
-                               '__**Downloaded :**__ __{percent}%__\n',
+                        a.edit(f'__**URL :**__ __{message.text}__\n'
+                               f'__**Total Size :**__ __{total_size} MB__\n'
+                               f'__**Downloaded :**__ __{percent}%__\n',
                                disable_web_preview=False)
                     except:
                         pass
                     if percent == 100:
                         show = 0
 
-        a.edit('__Downloaded to the server!\n'
-               'Uploading to Telegram Now ⏳__')
+        a.edit(f'__Downloaded to the server!\n'
+               f'Uploading to Telegram Now ⏳__')
         start = time.time()
         title = filename
         app.send_document(chat_id=message.chat.id,
                           document=f"./{directory}/{filename}",
                           caption=f"**File :** __{filename}__\n"
-                          "**Size :** __{total_size} MB__\n\n"
-                          "__Uploaded by @{BOT_URL}__",
+                          f"**Size :** __{total_size} MB__\n\n"
+                          f"__Uploaded by @{BOT_URL}__",
                           file_name=f"{directory}",
                           parse_mode='md',
                           progress=progress,
